3ddaa791e16117c2eb952f6f87f9e2ebbc4531ee
符现超 <fuxianchao@gmail.com>
2016-09-26 13:59:16 +0800

    adapte

--- Swift1/Playground/ZDPlayground.playground/Contents.swift
+++ Swift1/Playground/ZDPlayground.playground/Contents.swift
@@ -5,6 +5,7 @@ import UIKit
 var str = "Hello, playground"
 
 /// Array是struct类型，即值类型。class是引用类型
+/// 值类型会自动执行 copy 操作
 var arr = [0, 0, 0]
 var newArr = arr
 arr[0] = 1
@@ -12,6 +13,9 @@ print(arr)
 print(newArr)
 
 let myView = UIView()
-myView.backgroundColor = UIColor.brownColor()
+myView.backgroundColor = UIColor.brown
 myView.frame = CGRect.zero
 myView.frame = CGRect(x: 20.0, y: 20.0, width: 100.0, height: 100.0)
+
+
+		
